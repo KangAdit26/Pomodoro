255585bcdc83abdf5abdd196e18071a8cdc5c570
KangAdit26 <145187906+KangAdit26@users.noreply.github.com>
2026-07-02 10:02:31 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Pomodoro/',
   plugins: [react()],
   test: {
-    globals: true,         
-    environment: 'jsdom',   
+    globals: true,
+    environment: 'jsdom',
     setupFiles: './setupTests.js',
   },
 });
